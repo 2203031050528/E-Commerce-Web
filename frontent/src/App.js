97d0aeaf2e84b3b517e4abae9d0b24559931a32a
Rahul Jangir <rahuljangir4368@gmail.com>
2024-12-25 21:18:50 +0530

    create frontend of e-commerece web

---
+++ frontent/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+// import ProductList from './features/product-list/ProductList';
+import Home from './page/Home';
+
+function App() {
+  return (
+    <div className="App">
+    <Home>
+
+    </Home>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ frontent/src/page/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../features/navbar.js/Navbar'
+import ProductList from '../features/product-list/ProductList'
+
+function Home ()  {
+  return (
+    <div>
+      <Navbar>
+        
+      </Navbar>
+      <ProductList>
+        
+      </ProductList>
+    </div>
+  )
+}
+
+export default Home
